f660a1697739d6e16e1bc3e87859245df68548a0
Yemoe Aung <108300573+yemoeaung1@users.noreply.github.com>
2024-03-01 22:59:31 -0500

    home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,33 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import HomePage from './pages/HomePage'
+import gif from '/itachi-shillouette-in-front-of-the-red-moon_800.gif'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    
+    <div>
+     <div
+      className="bg-gradient-to-tr from-yellow-300 to-green-600"
+      // style={{
+      //   backgroundImage: `url(${gif})`,
+      //   backgroundSize: "cover",
+      //   height: "100vh",
+      // }}
+    ><HomePage /></div>
+    </div>
   )
 }
 
