1109511e86fa8f6339fa288a052c17d5693d1bfe
Yemoe Aung <108300573+yemoeaung1@users.noreply.github.com>
2024-03-03 00:45:42 -0500

    home page renovated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,12 +5,12 @@ import "./App.css";
 import FeedbackPage from "./feedbackPage";
 import HomePage from "./pages/HomePage";
 import gif from "/itachi-shillouette-in-front-of-the-red-moon_800.gif";
-import VideoPage from './videoPage'
-import TextToSpeech from './textToSpeech'
-import axios from 'axios'
+import VideoPage from "./pages/videoPage";
+import TextToSpeech from "./textToSpeech";
+import axios from "axios";
 import Model from "./threeDmodel";
 import { Routes, Route, Router, BrowserRouter } from "react-router-dom";
-
+import LoadingPage from "./pages/LoadingPage";
 
 function App() {
   // axios.defaults.withCredentials = true
@@ -20,7 +20,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/practice" element={<VideoPage />} />
-          <Route path="/mock" element={<VideoPage />} />
+          <Route path="/loading" element={<LoadingPage />} />
+          <Route path="/feedback" element={<FeedbackPage />} />
         </Routes>
       </BrowserRouter>
     </div>
